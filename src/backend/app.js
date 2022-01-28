ef2e01bc7141c5f640fd9c9ae367594475dcf661
Archit Jain <arcyjain2002@gmail.com>
2022-01-28 13:42:33 +0530

    dbms connect

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,9 +5,40 @@ const http = require("http");
 const fs = require("fs");
 const server = http.createServer(app);
 const path = require("path");
-
+const { find } = require("async");
 // middleware required
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.json());
+
+// hostname and port
+// const hostname = "0.0.0.0"; // ignore for deployment
+const port = process.env.PORT || 3000;
+
+//database connections
+const mongoose = require("mongoose");
+// models import
+// const Event = require("./models/events");
+
+// connect to the mongodb database
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/geofriends", {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("mongodb connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// listening to the server
+server.listen(port, () => {
+  console.log(`server running`);
+  // console.log(`server running `);
+});
