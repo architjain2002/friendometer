ff655c368e77962fc03376c7360d5b4242c401e9
Archit Jain <arcyjain2002@gmail.com>
2022-01-29 01:59:10 +0530

    authentication

--- src/reactApp/geofriends/src/App.js
+++ src/reactApp/geofriends/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper fadeInDown">
+        <div id="formContent">
+          {/* <!-- Tabs Titles --> */}
+          <h2 className="active"> Sign In </h2>
+          <h2 className="inactive underlineHover">Sign Up </h2>
+
+          {/* <!-- Icon --> */}
+          <div className="fadeIn first">
+            <img
+              src="http://danielzawadzki.com/codepen/01/icon.svg"
+              id="icon"
+              alt="User Icon"
+            />
+          </div>
+
+          {/* <!-- Login Form --> */}
+          <form>
+            <input
+              type="text"
+              id="login"
+              className="fadeIn second"
+              name="login"
+              placeholder="login"
+            />
+            <input
+              type="text"
+              id="password"
+              className="fadeIn third"
+              name="login"
+              placeholder="password"
+            />
+            <input type="submit" className="fadeIn fourth" value="Log In" />
+          </form>
+
+          {/* <!-- Remind Passowrd --> */}
+          <div id="formFooter">
+            <a className="underlineHover" href="#">
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
